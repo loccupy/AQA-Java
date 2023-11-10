20465ad1f0f5db364579e7fe31196f7175c001d0
Alexander <81022793+loccupy@users.noreply.github.com>
2023-11-10 20:47:51 +0300

    Initial commit

---
+++ Task1/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static int getRandomNumber() {
+        return (int) (Math.random()*500);
+    }
+    public static void main(String[] args) {
+        List <Integer> list = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            list.add(getRandomNumber());
+        }
+        System.out.println(list) ;
+        Stream<Integer> stream = list.stream();
+        System.out.println(stream.filter(z-> z%2 == 0).count());
+    }
+}
