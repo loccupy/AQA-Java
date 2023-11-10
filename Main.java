d93b02f5ca6abe4415abd67289ab08987ff5a52a
loccupy <nas121187@yandex.ru>
2023-11-10 23:06:23 +0300

    Main

--- Main.java
+++ Main.java
@@ -1,15 +1,47 @@
 public class Main{
-    
+
+    public static void printThreeWords(){
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void printColor(){
+        int value = 101;
+        if(value <= 0){
+            System.out.println("Красный");
+        }else if(value > 0 && value <= 100){
+            System.out.println("Желтый");
+        }else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 1, b = 2;
+        if(a >= b){
+         System.out.println("a >= b");
+        }else {
+          System.out.println("a < b");
+        }
+    }
+
+    public static void checkSumSing() {
+        int a = 9, b = -10;
+        if(a + b >= 0){
+            System.out.println("Сумма положительная");
+        }else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
     public static void main(String[] args) {
-        CheckSumSing.checkSumSing();
+        
         System.out.println("\n");
-        PrintThreeWords.printThreeWords();
+        printThreeWords();
         System.out.println("\n");
-        CheckSumSing.checkSumSing();
+        checkSumSing();
         System.out.println("\n");
-        PrintColor.printColor();
+        printColor();
         System.out.println("\n");
-        CompareNumbers.compareNumbers();
+        compareNumbers();
         System.out.println("\n");
      }
 }
