fcaf478b96c21eccb9cd114eeb1db2856043f18b
loccupy <nas121187@yandex.ru>
2023-11-23 18:41:09 +0300

    second

--- repo/Main.java
+++ repo/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         AppData obj = new AppData();
-      //  AppData obj2 = new AppData(new String[] {"Value 1", "Value 2", "Value 3"}, new int[][]{{100,200,123}, {300,400,500}});
         try {
             obj.readFileCsv();
             for(String temp: obj.getHeader()){
