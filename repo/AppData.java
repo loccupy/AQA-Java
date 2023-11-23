6586b335fb9d0c569e231caac6cb1b975d1a678d
loccupy <nas121187@yandex.ru>
2023-11-23 21:29:07 +0300

    3

--- repo/AppData.java
+++ repo/AppData.java
@@ -1,7 +1,7 @@
 package repo;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,11 +18,11 @@ public class AppData {
         this.data = data;
     }
 
-    public void readFileCsv () throws FileNotFoundException, IOException{
+    public void readFileCsv (File filename) throws IOException{
         int x = 0;
         String line;
         StringBuilder str = new StringBuilder("");
-        try(BufferedReader reader = new BufferedReader(new FileReader("repo/file.csv"))){
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             while((line = reader.readLine()) != null) {
                 if(x == 0) header = line.split(";");
                 else str.append(line + ";"); 
@@ -39,30 +39,43 @@ public class AppData {
                 q++;
             }
         }
+        System.out.println("Выполнена загрузка данных из csv файла!");
     }
 
-    public void writeFile1Csv () throws IOException {
-
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("repo/file1.csv"))){
+    public void writeFile1Csv (File filename) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             writer.write(String.join(";", header));
             writer.write("\n"); 
             int i = 0;
-            for(int z = 0;z < data.length;z++){
-                for(i = 0;i < header.length;i++){
-                    writer.write(Integer.toString(data[z][i]));
-                    if(i != header.length - 1) writer.write(";");
-
-                }
+            try {
+                for(int z = 0;z < data.length;z++){
+                    for(i = 0;i < data[0].length;i++){
+                        writer.write(Integer.toString(data[z][i]));
+                        if(i != header.length - 1) writer.write(";");
+                    }
                 if(z != data.length - 1)writer.write("\n");
+                }
+                System.out.println("Выполнено сохранение данных в csv файл!");
+            } catch (ArrayIndexOutOfBoundsException a){
+                System.out.println("Ошибка вводных данных!");
             }
         }
+        
     }
 
     public String [] getHeader(){
         return header;
     }
 
+    public void setHeader(String [] header){
+        this.header = header;
+    }
+
     public int [][] getData(){
         return data;
+    }
+
+    public void setData(int [][] data){
+        this.data = data;
     }
 }
--- repo/Main.java
+++ repo/Main.java
@@ -1,5 +1,6 @@
 package repo;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -7,18 +8,35 @@ public class Main {
 
     public static void main(String[] args) {
         AppData obj = new AppData();
+        String[] replaceHeader = new String[]{"First", "Second", "Third"};
+        int[][] replaceData = new int[][] {{11, 33, 55},{22, 44, 66}};
+        AppData obj1 = new AppData(new String[]{"Test 1", "Test 2", "Test 3"}, new int[][]{{111, 333, 555},{222, 444,666}});
+        
         try {
-            obj.readFileCsv();
-            for(String temp: obj.getHeader()){
-                System.out.print(temp + " ");
-            }
-            System.out.println("\n");
+            System.out.println("\n***Тестируем первый конструктор:***\n");
+            File filename = new File("repo/file.csv");
+            obj.readFileCsv(filename);
+            System.out.print("Проверяем содержимое header: ");
+            System.out.println(Arrays.toString(obj.getHeader()));
+            System.out.print("Проверяем содержимое data: ");
             System.out.println(Arrays.deepToString(obj.getData()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            obj.writeFile1Csv();
+            System.out.println("Меняем данные.");
+            obj.setHeader(replaceHeader);
+            obj.setData(replaceData);
+            System.out.print("Проверяем содержимое header: ");
+            System.out.println(Arrays.toString(obj.getHeader()));
+            System.out.print("Проверяем содержимое data: ");
+            System.out.println(Arrays.deepToString(obj.getData()));
+            obj.writeFile1Csv(filename);
+            System.out.println("*******************************************\n");
+            System.out.println("***Тестируем второй конструктор:***\n");
+            File filenameTest = new File("repo/newFile.csv");
+            System.out.print("Проверяем содержимое header: ");
+            System.out.println(Arrays.toString(obj1.getHeader()));
+            System.out.print("Проверяем содержимое data: ");
+            System.out.println(Arrays.deepToString(obj1.getData()));
+            obj1.writeFile1Csv(filenameTest);
+            System.out.println("*******************************************\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
