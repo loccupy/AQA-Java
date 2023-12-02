a052a4fadce9614bc29f8e095e6ea11c7a692ac4
loccupy <nas121187@yandex.ru>
2023-12-03 00:25:42 +0300

    Initial commit

---
+++ Task4/src/Main.java
@@ -0,0 +1,83 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Collection<Student> students = Arrays.asList(
+                new Student("Дмитрий", 17, Gender.MAN),
+                new Student("Максим", 20, Gender.MAN),
+                new Student("Екатерина", 20, Gender.WOMAN),
+                new Student("Михаил", 28, Gender.MAN)
+        );
+
+        System.out.println("\nЭтим студентам грозит получение повестки в этом году:");
+        students.stream().filter(x -> x.gender.equals(Gender.MAN))
+                .filter(x -> x.age >= 18 && x.age <= 27).forEach(System.out::println);
+
+
+        double res = students.stream()
+                .filter(x -> x.gender.equals(Gender.MAN))
+                .map(num -> Double.parseDouble(String.valueOf(num.age())))
+                .reduce(0.0, Double::sum);
+        res /= students.stream().filter(x -> x.gender.equals(Gender.MAN)).count();
+        System.out.printf("\nСредний возраст студентов мужского пола: %.4s лет.\n", res);
+    }
+
+    private enum Gender {
+        MAN,
+        WOMAN
+    }
+
+    private static final class Student {
+        private final String name;
+        private final Integer age;
+        private final Gender gender;
+
+        private Student(String name, Integer age, Gender gender) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+            @Override
+            public String toString() {
+                return "{" +
+                        "name='" + name + '\'' +
+                        ", age=" + age +
+                        ", gender=" + gender +
+                        '}';
+            }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Student)) return false;
+            Student student = (Student) o;
+            return Objects.equals(name, student.name) &&
+                    Objects.equals(age, student.age) &&
+                    Objects.equals(gender, student.gender);
+        }
+
+        public String name() {
+            return name;
+        }
+
+        public Integer age() {
+            return age;
+        }
+
+        public Gender gender() {
+            return gender;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age, gender);
+        }
+
+
+        }
+
+}
