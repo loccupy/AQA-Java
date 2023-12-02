c09ce229e3aa5a1cf9e4e3c090538f09d75ffab2
loccupy <nas121187@yandex.ru>
2023-12-02 13:37:57 +0300

    Initial commit

---
+++ Task2/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+
+    public static void main(String[] args) {//"Highload", "High", "Load", "Highload"
+        ArrayList<String> list = new ArrayList<>(List.of("Highload", "High", "Load", "Highload"));
+        System.out.println("Исходный массив:\n" + list + "\n");
+        System.out.println("Cколько раз объект «High» встречается в коллекции:\n   " +
+                            countHigh(list));
+        System.out.println("\nКакой элемент в коллекции находится на первом месте:\n   " +
+                            firstElement(list));
+        System.out.print("\nКакой элемент в коллекции находится на последнем месте:\n   " +
+                            lastElement(list));
+    }
+
+    public static Object countHigh(List<String> list) {
+        if(!list.isEmpty()){
+            return list.stream().filter(x -> x.equals("High")).count();
+        }
+            return 0;
+    }
+
+    public static Object firstElement(List<String> list) {
+            return list.stream().findFirst().orElse(String.valueOf(0));
+    }
+
+    public static Object lastElement(List<String> list) {
+        return list.stream().reduce((a1,a2) -> a2).orElse(String.valueOf(0));
+    }
+}
