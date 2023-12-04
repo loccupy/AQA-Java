3c581b8972da7b4efeebb764d4bec6bb72f086c0
loccupy <nas121187@yandex.ru>
2023-12-04 11:53:07 +0300

    Initial commit

---
+++ Task5/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList <String> listOfLogins = new ArrayList<>();
+        enterLogin(listOfLogins);
+        System.out.println("Список логинов, начинающихся с 'f':");
+        listOfLogins.stream()
+                    .filter(x -> x.charAt(0) == 'f')
+                    .forEach(x -> System.out.println("     " + x));
+    }
+
+    public static void enterLogin(ArrayList <String> listOfLogins) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите логин: ");
+        while(true) {
+            String tmp = sc.nextLine();
+            if(tmp.isEmpty()){
+                break;
+            } else {
+                listOfLogins.add(tmp);
+            }
+        }
+    }
+}
