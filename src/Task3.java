7372a8a4b903b9f0adf285910078c036094307fb
loccupy <nas121187@yandex.ru>
2023-12-10 19:47:06 +0300

    перемешал элементы

--- src/Task3.java
+++ src/Task3.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Task3 {
 
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>(List.of("f10", "f15", "f2", "f4", "f4"));
+        ArrayList<String> list = new ArrayList<>(List.of("f4", "f2", "f4", "f10", "f15"));
         System.out.println("\nИсходная коллекция: " + list);
         String[] result =  list.stream().sorted().toArray(String[]::new);
         System.out.println("\nМассив после сортировки: " + Arrays.toString(result));
